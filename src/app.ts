15cbb74072e91135d7b768976644abb4aeef1961
YokoHayakawa <kaba3kobu@gmail.com>
2023-12-21 14:32:44 +0900

    init

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+
+
+
+const onIndexShow = [
+  'app.record.index.show',
+  'mobile.app.record.index.show',
+];
+
+
+(async () => {
+  console.log(`Running in ${isProd ? 'production' : 'development'}`);
+  kintone.events.on(onIndexShow, onIndexShowHandler);
+
+})();
