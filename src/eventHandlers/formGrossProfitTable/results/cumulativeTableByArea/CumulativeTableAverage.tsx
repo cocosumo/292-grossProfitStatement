416d6d70334dde520277a2bfd759a1d6b96eac29
YokoHayakawa <kaba3kobu@gmail.com>
2023-12-27 11:39:07 +0900

    Update CumulativeTableAverage.tsx

--- src/eventHandlers/formGrossProfitTable/results/cumulativeTableByArea/CumulativeTableAverage.tsx
+++ src/eventHandlers/formGrossProfitTable/results/cumulativeTableByArea/CumulativeTableAverage.tsx
@@ -1,6 +1,11 @@
+import {type SummaryContracts} from '@/eventHandlers/helpers/getSummaryContracts';
 
 /** 対象期間の一人当たりの平均表 */
-export const CumulativeTableAverage = () => (
+export const CumulativeTableAverage = ({
+	contractData,
+}: {
+	contractData: SummaryContracts[];
+}) => (
 	<>
       対象期間の一人当たりの平均
 		<br />
