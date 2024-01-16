bf22084d24b00d531cd547f526a86e260ac552ea
YokoHayakawa <kaba3kobu@gmail.com>
2024-01-16 12:09:05 +0900

    Update GrossProfitByPerson.tsx

--- src/eventHandlers/formGrossProfitTable/results/grossProfitByPerson/GrossProfitByPerson.tsx
+++ src/eventHandlers/formGrossProfitTable/results/grossProfitByPerson/GrossProfitByPerson.tsx
@@ -1,13 +1,43 @@
 import {Stack} from '@mui/material';
 import {type SummaryContracts} from '../../../helpers/getSummaryContracts';
+import {type IEmployees} from '@api/getEmployees';
+import {useAreaNameById} from '../hooks/useAreaNameById';
+import {getMonthsNum} from '../helper/getMonthsNum';
+import {useMemo} from 'react';
+import {getMembers} from '../helper/getMembers';
 
 /** 担当者ごとの粗利表を表示する */
 export const GrossProfitByPerson = ({
-	contractData,
+	area,
+	periods,
+	year,
+	summaryContracts,
+	employees,
 }: {
-	contractData: SummaryContracts[];
-}) => (
-	<Stack spacing={2}>
+	area: string[];
+	periods: string[];
+	year: string;
+	summaryContracts: SummaryContracts[];
+	employees: IEmployees[] | undefined;
+}) => {
+	const storeNames = useAreaNameById(area);
+	const monthsNum = getMonthsNum(periods);
+	const tableLabel = '担当者別粗利表';
+
+	const members = useMemo(() => {
+		if (!employees) {
+			return [] as IEmployees[];
+		}
+
+		return getMembers({
+			employees,
+			area,
+		});
+	}, [employees, area]);
+
+	return	(
+		<Stack spacing={2}>
       担当者ごとの粗利表
-	</Stack>
-);
+		</Stack>
+	);
+};
